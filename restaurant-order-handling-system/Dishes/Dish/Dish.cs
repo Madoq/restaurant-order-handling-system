04ca526098ce66de884eefc5de5d12fd70f576e5
mikolajsniechowski <mikolaj.sniechowski@stud.pwsz.legnica.edu.pl>
2022-01-10 17:39:16 +0100

    vegan interface

--- restaurant-order-handling-system/Dishes/Dish/Dish.cs
+++ restaurant-order-handling-system/Dishes/Dish/Dish.cs
@@ -6,19 +6,44 @@ using System.Threading.Tasks;
 
 namespace restaurant_order_handling_system
 {
-    abstract class Dish
+    abstract class Dish : IisVegan
     {
         public string Name { get; }
         public double Price { get; }
         public List<Ingredient> Ingredients { get; }
         public int Weight { get; }
-
+        public bool ifisvegan {get;set;}
         public Dish(string name, double price, List<Ingredient> ingredients, int weight)
         {
             Name = name;
             Price = price;
             Ingredients = ingredients;
             Weight = weight;
+        }
+
+        internal string DishToString()
+        {
+            throw new NotImplementedException();
+        }
+        public void isvegan(Boolean bl )
+        {
+            ifisvegan = bl;
+        }
+         public String DishToStringSave()
+        {
+            return this.Name + ";" + Convert.ToString(this.Price) + ";" +IngredientsToString(this.Ingredients)+";"+this.Weight+";"+this.ifisvegan;
+        }
+
+        public String IngredientsToString(List<Ingredient> list)
+        {
+            String ingredients = "";
+            foreach (Ingredient ingredient in list)
+            {
+                ingredients += ingredient.name;
+                ingredients += ",";
+            }
+            ingredients = ingredients.Remove(ingredients.Length - 1);
+            return ingredients;
         }
     }
 }
